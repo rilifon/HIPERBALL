05704124e923118af6005673caa7dc12b5c61705
rilifon <pudimdefrutas@gmail.com>
2016-04-27 18:20:31 -0300

    FIXED BUG FOR TELEPORTATION
    Sometimes Godot physics would overlay set_pos, so fixed that with a flag

--- game.gd
+++ game.gd
@@ -1,14 +1,23 @@
 
 extends Node2D
 
+var teleport = false
+
 func _ready():
 	set_process_input(true)
+	set_fixed_process(true)
 
 func start():
-	pass
-
-func _input(event):
 	var ball = get_node("ball")
 	ball.set_pos(Vector2(250,300))
 	ball.set_linear_velocity(Vector2(0,0))
+	teleport = false
+
+func _fixed_process(delta):
+	if teleport:
+		start()
+		
+func _input(event):
+	teleport = true
+
 
